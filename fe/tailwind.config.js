488dce451df62c3b47a577483d5aa77cb416526c
Thuong <nguyenvanthuong10112003@gmail.com>
2025-12-06 10:54:47 +0700

    first commit

---
+++ fe/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: '30rem'
+      }
+    },
+  },
+  plugins: [],
+};
